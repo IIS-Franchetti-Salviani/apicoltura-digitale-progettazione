32729295f3ff2e870ea55a3aa9f1eb74bf854b2b
Francesco Adriani <francesco.adriani@franchettisalviani.net>
2026-04-08 22:28:12 +0200

    riallineato main

--- src/esp/esp.ino
+++ src/esp/esp.ino
@@ -1251,4 +1251,7 @@ void stampaStatistiche() {
   Serial.print("Wi-Fi:  "); Serial.println(isWiFiConnected() ? "Connesso" : "Disconnesso");
   if (isWiFiConnected()) {
     Serial.print("SSID: "); Serial.println(WiFi.SSID());
-    Serial.print("RSSI:
+    Serial.print("RSSI: "); Serial.print(WiFi.RSSI()); Serial.println(" dBm");
+  }
+  Serial.println();
+}
